215238b24094c71dec3ca1026bce9c7ce7ec5846
Alexey Svetliakov <asvetliakov@teledini.com>
2018-07-09 00:18:15 +0200

    added rest parameters to TS signature

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 /**
  * Create mock instance of given class or function constructor
- * 
+ *
  * @param classConstructor Class constructor
  * @returns New instance of given class constructor with all methods being mocked
  */
-export function createMockInstance<T>(classConstructor: { new(...args: any[]): T } | { (): T }): jest.Mocked<T>;
+export function createMockInstance<T>(classConstructor: { new(...args: any[]): T } | { (...args: any[]): T }): jest.Mocked<T>;
 
 export default createMockInstance;
